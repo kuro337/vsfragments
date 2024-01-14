9757f2293a2ae1cb95423c15b89e29aedb1e8e76
kuro337 <cb.kalz@gmail.com>
2024-01-13 23:14:23 -0500

    e2e flows

--- napi/lib/export.js
+++ napi/lib/export.js
@@ -1,8 +1,5 @@
 const addon = require("node-gyp-build")(__dirname);
 
-// console.log(__dirname);
-// console.log("Addon loaded:", addon); // This logs the loaded addon object
-
 module.exports = {
   createSnippetWithMetadata: addon.createSnippetWithMetadata,
 
@@ -13,15 +10,3 @@ module.exports = {
   parseStringFromNode: addon.parseStringFromNode,
   getPath: addon.getPath,
 };
-
-function testOne() {
-  const inputString = "asdajsnd\\n\\\\nnnnasdkaskmaskdm";
-  const directResult = addon.parseStringFromNode(inputString);
-
-  const expectedOutput =
-    '\t"VSCode Code Snippet": {\n\t\t"prefix": "prefix_insertSnippet",\n\t\t"body": [\n\t\t\t"asdajsnd\\\\n\\\\\\\\\\\\nnnnasdkaskmaskdm"\n\t\t],\n\t\t"description": "Some Useful Snippet Descriptor. Pass --desc <string> to set explicitly."\n\t}\n';
-
-  if (directResult === expectedOutput) console.log("EQUAL");
-}
-
-testOne();
