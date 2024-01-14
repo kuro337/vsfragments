137e6a0b89d1bdbe945a66758b8b64baf6a1e4c8
kuro337 <cb.kalz@gmail.com>
2024-01-14 01:25:36 -0500

    e2e

--- napi/scripts/PREBUILT.sh
+++ napi/scripts/PREBUILT.sh
@@ -2,20 +2,24 @@
 
 IMAGE="linux_napi"
 CONTAINER="linux_pod"
-CONTAINER_PATH="/app/vsfragments/napi/lib/prebuilds"
-DEST_PATH_A="../../napi/lib/prebuilds"
-DEST_PATH_B="../../napi/prebuilds"
+SRC_PATH_A="/app/vsfragments/napi/prebuilds"
+SRC_PATH_B="/app/vsfragments/napi/lib/prebuilds"
+DEST_PATH_A="../lib/prebuilds"
+DEST_PATH_B="../prebuilds"
 
 # run build container
 podman build -t $IMAGE .  --no-cache
 podman run --name $CONTAINER $IMAGE 
 
 # Create the destination directory if it does not exist
-mkdir -p "$DEST_PATH"
+mkdir -p "$DEST_PATH_A"
+mkdir -p "$DEST_PATH_B"
 
 # Copy the contents of the folder from the container to dest outside container
-podman cp "$CONTAINER:$CONTAINER_PATH/." "$DEST_PATH_A"
-podman cp "$CONTAINER:$CONTAINER_PATH/." "$DEST_PATH_B"
+podman cp "$CONTAINER:$SRC_PATH_A/." "$DEST_PATH_A"
+podman cp "$CONTAINER:$SRC_PATH_A/." "$DEST_PATH_B"
+podman cp "$CONTAINER:$SRC_PATH_B/." "$DEST_PATH_A"
+podman cp "$CONTAINER:$SRC_PATH_B/." "$DEST_PATH_B"
 
 echo "Folder copied to $DEST_PATH_A"
 echo "Folder copied to $DEST_PATH_B"
